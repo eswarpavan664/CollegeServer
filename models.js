0f259b7feae5ab3e8d12dff7d54f2cde75b55a63
pavaneswar kothalanka <itonystark58@gmail.com>
2022-09-28 23:48:24 +0530

    make it

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
+
+
 const StudentuserSchema = new mongoose.Schema({
     email:{
         type:String,
